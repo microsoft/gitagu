cfb59ebe0d38efe02aa7f0db23d39128744ba33e
Govind Kamtamneni <gok@microsoft.com>
2025-07-08 23:02:16 -0700

    add replit async agent

--- frontend/src/data/agents/index.ts
+++ frontend/src/data/agents/index.ts
@@ -4,11 +4,13 @@ import githubCopilotAgent from './github-copilot-agent';
 import devin from './devin';
 import codexCli from './codex-cli';
 import sreAgent from './sreagent';
+import replit from './replit';
 
 const agents: Agent[] = [
   githubCopilotCompletions,
   githubCopilotAgent,
   devin,
+  replit,
   codexCli,
   sreAgent
 ];
